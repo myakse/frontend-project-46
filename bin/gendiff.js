f97abbddc78c68134c0d8c4783974af9de7709d2
Ksunya <Ksenia.Yakimenko.6@yandex.ru>
2023-04-28 16:38:54 +0400

    Name of commit

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
+import gendiff from '../scr/index.js';
+
 
 const program = new Command();
 
@@ -8,5 +10,10 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
+  .option('-f, --format <type>', 'output format')
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => {
+    console.log(gendiff(filepath1, filepath2, program.opts().format));
+  });
 
 program.parse();
---
+++ scr/index.js
@@ -0,0 +1,34 @@
+import { readFileSync } from 'fs';
+import _ from "lodash";
+ 
+const gendiff = (file1, file2) => {
+    const keys = _.sortBy(_.union(_.keys(file1), _.keys(file2)));
+        
+    const result = keys.map((key) => {
+        if (!_.has(file1, key)) {
+            return `${'+'} ${key} : ${file2[key]}`;
+        }
+        if (!_.has(file2, key)) {
+            return `${'-'} ${key} : ${file1[key]}`;
+        }
+        if (file1[key] === file2[key]) {
+            return `  ${key} : ${file1[key]}`;
+        }        
+        if (file1[key] !== file2[key]) {
+            return [`${'-'} ${key} : ${file1[key]}`,
+            `${'+'} ${key}: ${file2[key]}`];
+        }
+    });
+    
+    return (`{\n ${result.flat().join('\n ')}\n}`);
+}; 
+
+export default (filepath1, filepath2) => {
+    const file1 = readFileSync(filepath1, 'utf-8');
+    const file2 = readFileSync(filepath2, 'utf-8');
+
+    const parseFile1 = JSON.parse(file1);
+    const parseFile2 = JSON.parse(file2);
+
+    return gendiff(parseFile1, parseFile2);
+};
